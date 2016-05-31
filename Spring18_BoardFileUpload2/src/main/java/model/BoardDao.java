558b5bf1dda280bac946d477fcb567f8765f40aa
yumin <cym6749@gmail.com>
2016-05-31 16:52:50 +0900

    xdvxdv

--- Spring18_BoardFileUpload2/src/main/java/model/BoardDao.java
+++ Spring18_BoardFileUpload2/src/main/java/model/BoardDao.java
@@ -12,6 +12,20 @@ public class BoardDao {
 		this.sqlSession = sqlSession;
 	}
 	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	public int write(BoardVO bvo) throws SQLException{
 		return sqlSession.insert("boardSql.write", bvo);
 	}
